fbe7445781702c2516e2eb235558d6be6aaeb006
陈伟 <wei.chen@3glasses.com>
2020-10-16 17:48:41 +0800

    同步更新

--- index.d.ts
+++ index.d.ts
@@ -1,23 +1,31 @@
 /*
  * @Description: 注释
- * @Date: 2020-09-05 14:44:28
- * @LastEditTime: 2020-09-05 21:36:23
+ * @Date: 2020-10-16 16:58:43
+ * @LastEditTime: 2020-10-16 17:04:58
  * @LastEditors: chenwei
  */
 import * as React from 'react';
+import { CropperProps } from 'react-easy-crop';
 
 export interface ImgCropProps {
   aspect?: number;
   shape?: 'rect' | 'round';
   grid?: boolean;
+  quality?: number;
+
   zoom?: boolean;
   rotate?: boolean;
-  beforeCrop?: (file: File, fileList: File[]) => boolean;
+  minZoom?: number;
+  maxZoom?: number;
   gifCrop?: boolean,
+
   modalTitle?: string;
   modalWidth?: number | string;
   modalOk?: string;
   modalCancel?: string;
+
+  beforeCrop?: (file: File, fileList: File[]) => boolean;
+  cropperProps?: Partial<CropperProps>;
 }
 declare const ImgCrop: React.FC<ImgCropProps>;
 export default ImgCrop;
